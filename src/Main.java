70be8bdbdabc57173dabb3a4cbfac70eb8e2ad7b
BT_3N3_05 <hyang2496@gmail.com>
2025-03-03 10:40:34 -0500

    Updated math random operations

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,10 @@ public class Main
     public static void main(String arg[]){
         Rectangle rectangle1 = new Rectangle(20,10,10,15);
         ArrayList<Rectangle>rectangleArrayList = new ArrayList<>();
-        for(int i = 0;i<999999;i++){
-            Rectangle newRectangle = new Rectangle((int)(Math.random()*50+10),
-                    (int)(Math.random()*10+5),(int)(Math.random()*49),
-                    (int)(Math.random()*49));
+        for(int i = 0;i<50;i++){
+            Rectangle newRectangle = new Rectangle((int)(Math.random()*41+10),
+                    (int)(Math.random()*6+5),(int)(Math.random()*49+1),
+                    (int)(Math.random()*49+1));
             rectangleArrayList.add(newRectangle);
             System.out.println("Rectangle " + (i+1) + "." +
                     " Length:" +rectangleArrayList.get(i).getLength() +
