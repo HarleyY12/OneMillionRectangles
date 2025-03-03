d951db9304f7165abf54060565ef09e9fec6846c
BT_3N3_05 <hyang2496@gmail.com>
2025-03-03 10:52:58 -0500

    Added variables to track percentage and number of total rectangles

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,10 @@ import java.util.ArrayList;
 public class Main
 {
     public static void main(String arg[]){
+        int numberOfTotalRectangles = 50;
         Rectangle rectangle1 = new Rectangle(20,10,10,15);
         ArrayList<Rectangle>rectangleArrayList = new ArrayList<>();
-        for(int i = 0;i<50;i++){
+        for(int i = 0;i<numberOfTotalRectangles;i++){
             Rectangle newRectangle = new Rectangle((int)(Math.random()*41+10),
                     (int)(Math.random()*6+5),(int)(Math.random()*49+1),
                     (int)(Math.random()*49+1));
@@ -18,9 +19,12 @@ public class Main
                     " X position:" + rectangleArrayList.get(i).getxPos() +
                     " Y position:" + rectangleArrayList.get(i).getyPos());
         }
-
-
-
+        double percentage = 0.0;
+        int touchingRectangles = 0;
+        for(int i =0;i<numberOfTotalRectangles;i++){
+        }
+        percentage = (double)(touchingRectangles/numberOfTotalRectangles)*100;
+        System.out.println(percentage);
     }
 
 
